86ad78bd91fe4fc071aec1868dcbf695d908ab18
adamsuherman <adam.as.suherman@gmail.com>
2024-10-03 16:36:44 +0700

    add gettransaction and update data

--- MCFTechnicaltest/MCFTechnicaltest/Controllers/Transaction.cs
+++ MCFTechnicaltest/MCFTechnicaltest/Controllers/Transaction.cs
@@ -48,8 +48,8 @@ namespace MCFTechnicaltest.Controllers
                 param.location_id = request.location_id;
                 param.created_by = request.created_by;
                 param.created_on = request.created_on;
-
                 _context.Add(param);
+
                 _context.SaveChanges();
                 result.ResultCode = "1";
                 result.ResultMessage = "SUCCESS";
@@ -60,6 +60,66 @@ namespace MCFTechnicaltest.Controllers
                 result.ResultMessage = ex.Message;
             }
             return result;
+        }
+
+        public async Task<ResultObject> UpdateData(tr_bpkb request)
+        {
+            ResultObject result = new ResultObject();
+            try
+            {
+                tr_bpkb param = new tr_bpkb();
+                param.agreement_number = request.agreement_number;
+                param.branch_id = request.branch_id;
+                param.bpkb_no = request.bpkb_no;
+                param.bpkb_date_in = request.bpkb_date_in;
+                param.bpkb_date = request.bpkb_date;
+                param.faktur_no = request.faktur_no;
+                param.faktur_date = request.faktur_date;
+                param.policy_no = request.policy_no;
+                param.location_id = request.location_id;
+                param.last_updated_by = request.created_by;
+                param.last_updated_on = request.created_on;
+                _context.Update(param);
+                _context.SaveChanges();
+
+                result.ResultCode = "1";
+                result.ResultMessage = "SUCCESS";
+            }
+            catch(Exception ex)
+            {
+                result.ResultCode = "0";
+                result.ResultMessage = ex.Message;
+            }
+            return result;
+        }
+        public async Task<tr_bpkb> GetTransactionData(string agreementNumber)
+        {
+            tr_bpkb data = new tr_bpkb();
+            try
+            {
+                var result = await _context.tr_bpkb.Where(t => t.agreement_number == agreementNumber).ToListAsync();
+                if(result.Count> 0)
+                {
+                    data.agreement_number = result[0].agreement_number;
+                    data.bpkb_no = result[0].bpkb_no;
+                    data.branch_id = result[0].branch_id;
+                    data.bpkb_date = result[0].bpkb_date;
+                    data.bpkb_date_in = result[0].bpkb_date_in;
+                    data.faktur_no = result[0].faktur_no;
+                    data.faktur_date = result[0].faktur_date;
+                    data.location_id = result[0].location_id;
+                    data.created_by = result[0].created_by;
+                    data.created_on = result[0].created_on;
+                    data.last_updated_by = result[0].last_updated_by;
+                    data.last_updated_on = result[0].last_updated_on;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return data;
         }
     }
 }
--- MCFTechnicaltest/MCFTechnicaltest/Controllers/TransactionController.cs
+++ MCFTechnicaltest/MCFTechnicaltest/Controllers/TransactionController.cs
@@ -15,6 +15,7 @@ namespace MCFTechnicaltest.Controllers
         private IConfiguration _configuration;
         private readonly Transaction _transaction;
         List<ms_storage_location> _msStorageLocation;
+        tr_bpkb _tr_bpkb;
         private readonly CodingTestContext _context;
         ResultObject result;
         public TransactionController(IConfiguration configuration, CodingTestContext context) 
@@ -31,10 +32,24 @@ namespace MCFTechnicaltest.Controllers
             return new OkObjectResult(_msStorageLocation);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetTransaction(string agreementnumber)
+        {
+            _tr_bpkb = await _transaction.GetTransactionData(agreementnumber);
+            return new OkObjectResult(_tr_bpkb);
+        }
+
         [HttpPost]
         public async Task<IActionResult> SaveTransaction(tr_bpkb request)
         {
             result = await _transaction.SaveData(request);
+            return new OkObjectResult(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateTransaction(tr_bpkb request)
+        {
+            result = await _transaction.UpdateData(request);
             return new OkObjectResult(result);
         }
     }
