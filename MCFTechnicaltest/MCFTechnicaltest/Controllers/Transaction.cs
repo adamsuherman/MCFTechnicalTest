3c6519e44c802211dcfc340a78eff6e8db91dd12
adamsuherman <adam.as.suherman@gmail.com>
2024-10-03 15:27:25 +0700

    Add Project Front End

--- MCFTechnicaltest/MCFTechnicaltest/Controllers/Transaction.cs
+++ MCFTechnicaltest/MCFTechnicaltest/Controllers/Transaction.cs
@@ -36,7 +36,20 @@ namespace MCFTechnicaltest.Controllers
             ResultObject result = new ResultObject();
             try
             {
-                _context.Add(request);
+                tr_bpkb param = new tr_bpkb();
+                param.agreement_number= request.agreement_number;
+                param.branch_id = request.branch_id;
+                param.bpkb_no = request.bpkb_no;
+                param.bpkb_date_in = request.bpkb_date_in;
+                param.bpkb_date = request.bpkb_date;
+                param.faktur_no = request.faktur_no;
+                param.faktur_date = request.faktur_date;
+                param.policy_no = request.policy_no;
+                param.location_id = request.location_id;
+                param.created_by = request.created_by;
+                param.created_on = request.created_on;
+
+                _context.Add(param);
                 _context.SaveChanges();
                 result.ResultCode = "1";
                 result.ResultMessage = "SUCCESS";
